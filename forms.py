b931594209535dd4557a8fdaa8a4668b2ea8e07d
sirln <siro@sirlawren.com>
2023-11-27 19:22:08 +0300

    version 3 of form handling/validation

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo
+
 
 class SignUpForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
@@ -10,17 +12,26 @@ class SignUpForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     # submit = SubmitField('Sign Up')
 
+
 class SignInForm(FlaskForm):
     username_email = StringField('Username/Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     # submit = SubmitField('Sign In')
 
+
 class ForgotPasswordForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     # submit = SubmitField('Submit')
+
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     # submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    description = TextAreaField('Description')
+    file_types = ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mkv']
+    file = FileField('File', validators=[FileRequired(), FileAllowed(file_types)])
